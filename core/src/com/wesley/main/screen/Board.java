63a290b8409eaa31b1ba2b60a290d63dd62a6218
Wesley Oz <wesleyoliveirap@gmail.com>
2019-11-26 22:54:36 -0300

    Restart game menu implemented

--- core/src/com/wesley/main/screen/Board.java
+++ core/src/com/wesley/main/screen/Board.java
@@ -3,15 +3,18 @@ package com.wesley.main.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -30,6 +33,7 @@ public class Board extends Screen {
     float deltaY = 0;
     Stage _stage;
     Button _btnRestart;
+    Table _restartDialogTable;
 
     public Board(int size) {
         super();
@@ -46,10 +50,47 @@ public class Board extends Screen {
         this._btnRestart.addListener(new ChangeListener() {
             @Override
             public void changed (ChangeEvent event, Actor actor) {
-                Board.this.startGame();
+                Board.this.buildRestartDialog();
             }
         });
 
+    }
+
+    private void buildRestartDialog() {
+        this._restartDialogTable = new Table();
+
+        BitmapFont font = new BitmapFont();
+        font.setColor(Color.BLACK);
+        font.getData().setScale(5);
+        Label.LabelStyle labelStyle = new Label.LabelStyle(font, Color.WHITE);
+        Label label = new Label("Restart Game?", labelStyle);
+        this._restartDialogTable.add(label).width(400);
+        this._restartDialogTable.row();
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = new BitmapFont();
+        TextButton btnNo = new TextButton("No", textButtonStyle);
+        btnNo.getLabel().setFontScale(5f);
+        btnNo.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Board.this._restartDialogTable = null;
+            }
+        });
+        this._restartDialogTable.add(btnNo).width(200);
+
+        TextButton btnYes = new TextButton("Yes", textButtonStyle);
+        btnYes.getLabel().setFontScale(5f);
+        btnYes.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Board.this.startGame();
+            }
+        });
+        this._restartDialogTable.add(btnYes).width(200);
+
+        this._restartDialogTable.setFillParent(true);
+        this._stage.addActor(this._restartDialogTable);
     }
 
     private void startGame() {
@@ -62,7 +103,7 @@ public class Board extends Screen {
         this._tiles.createTile();
         this._tiles.createTile();
         this._playerTurn = true;
-
+        this._restartDialogTable = null;
         this._font = new BitmapFont();
         this._font.setColor(Color.WHITE);
         this._font.getData().setScale(5);
@@ -70,10 +111,17 @@ public class Board extends Screen {
 
     public Screen update() {
 
+        if (this._restartDialogTable != null) {
+            if (Gdx.input.isKeyJustPressed(Input.Keys.BACK)) {
+                this._restartDialogTable = null;
+            }
+            return this;
+        }
+
         if (Gdx.input.isKeyJustPressed(Input.Keys.BACK)) {
             return new MainMenu();
         }
-        
+
         if (this._tiles.getState() != Tiles.STATE.STATIC) {
             this._tiles.update();
             this._playerTurn = false;
@@ -131,6 +179,22 @@ public class Board extends Screen {
         this._font.draw(super._spriteBatch, text,100,100);
         this._btnRestart.draw(super._spriteBatch, 1);
         super._spriteBatch.end();
+        this.drawRestartDialog();
+    }
+
+    private void drawRestartDialog() {
+
+        if (this._restartDialogTable == null) return;
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        super._shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        super._shapeRenderer.setColor(1f, 0.7f, 0.2f, 0.8f);
+        super._shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        super._shapeRenderer.end();
+        super._spriteBatch.begin();
+        this._restartDialogTable.draw(super._spriteBatch, 1);
+        super._spriteBatch.end();
+        Gdx.gl.glDisable(GL20.GL_BLEND);
     }
 
     private void drawBGRect(int x, int y) {
@@ -152,6 +216,7 @@ public class Board extends Screen {
 
     public void dispose() {
         super.dispose();
+
         this._tiles.dispose();
     }
 }
